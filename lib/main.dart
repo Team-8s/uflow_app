1c9cb0c9987ea4abe4012b9bda26d93566be1e32
Jesse Phipps <jessefphipps@gmail.com>
2021-04-07 15:26:44 -0500

    Added Bluetooth Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:uflow_app/register_user_screen.dart';
 import 'input_page.dart';
 import 'login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(InitApp());
 
@@ -34,15 +35,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      // home: MyStatefulWidget(),
-            initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/register-user': (context) => RegisterUserScreen(),
-        '/home': (context) => MyStatefulWidget(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<EMGData>(create: (_) => EMGData(),),
+      ],
+          child: MaterialApp(
+        
+        title: _title,
+        // home: MyStatefulWidget(),
+              initialRoute: '/',
+        routes: {
+          '/': (context) => LoginScreen(),
+          '/register-user': (context) => RegisterUserScreen(),
+          '/home': (context) => MyStatefulWidget(),
+        },
+      ),
     );
+  }
+}
+
+class EMGData with ChangeNotifier {
+  int _emg1 = 0;
+  int _emg2 = 0;
+
+  EMGData({emg1, emg2});
+
+  get emg1 => _emg1;
+
+  get emg2 => _emg2;
+
+  set emg1(int value){
+    _emg1 = value;
+    notifyListeners();
+  }
+
+  set emg2(int value){
+    _emg2 = emg2;
+    notifyListeners();
   }
 }
